6bdd66d3eea52d60c8b77f1118c2f52a2290b6c2
Cody Evans <1123code@gmail.com>
2020-05-11 00:19:15 -0700

    Named some stuff differently

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -89,9 +89,11 @@ TEST(PiezasTest, xWinsGame) {
 	board.dropPiece(2);
 	board.dropPiece(2);
 	Piece testPiece;
+	Piece testPiece2;
 	testPiece = board.gameState();
+	testPiece2 = board.gameState();
 	ASSERT_EQ(testPiece, X);
-	ASSERT_EQ(board.gameState(), Invalid);
+	ASSERT_EQ(testPiece2, Invalid);
 }
 
 TEST(PiezasTest, oWinsGame) {
